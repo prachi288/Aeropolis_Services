b5e2e48d4ec7bd0dde6f89a0346dca3cd9f3a436
prachi288 <200~96807804+prachi288@users.noreply.github.com>
2023-07-26 21:39:04 +0530

    Added basic Airplane model and migration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express= require('express');
 
-const {ServerConfig,Logger}=require('./config');
+const {ServerConfig}=require('./config');
 const apiRoutes= require('./routes');
-const serverConfig = require('./config/server-config');
 
 const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/api',apiRoutes);
 
 app.listen(ServerConfig.PORT,()=>{
-    console.log(`Successfully started the server on PORT: ${PORT}`);
-    Logger.info("successfully started the server",{msg: "something"});
+    console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
 });
