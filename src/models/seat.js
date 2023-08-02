8fc47b1de5a2b3e94fdd4c04ee4554b7c114e87b
prachi288 <200~96807804+prachi288@users.noreply.github.com>
2023-08-03 00:40:32 +0530

    Added associations

--- src/models/seat.js
+++ src/models/seat.js
@@ -14,6 +14,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.Airplane, {
+        foreignKey: 'airplaneId',
+      });
     }
   }
   Seat.init({
